92aa0fe74f44229f790b070e978f937e31543d67
Chidori Shiori <160299866+FengIKUN@users.noreply.github.com>
2026-06-28 19:06:48 +0800

    Refactor Vite config to use defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default {
-  base: '/Political-Test/'
-}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/Political-Test/'
 })
